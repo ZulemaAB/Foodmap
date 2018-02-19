d63fdb4b48e90fe2535a4343c1961ac05a62b9ff
ZulemaAB <malezu06@hotmail.com>
2018-02-19 12:01:53 -0500

    modificando seleccion de restaurantes

---
+++ js/options.js
@@ -0,0 +1,17 @@
+// Función para mostrar todas las alternativas de restaurante
+$(document).ready(function() {
+  $('ul.listado li a').click(function() {
+    var textoFiltro = $(this).text().toLowerCase().replace(/\s/g, '-');
+    if (textoFiltro === 'todos') {
+      $('div.filtros div.hidden').fadeIn('slow').removeClass('hidden');
+    } else {
+      $('.filtros div').each(function() {
+        if (!$(this).hasClass(textoFiltro)) {
+          $(this).fadeOut('normal').addClass('hidden');
+        } else {
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    } return false;
+  });
+});
